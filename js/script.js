689cdcc653a9a3f36bed1bcde469c57ebd2a76e7
Caio-Santana <caio.santana@live.com>
2022-07-18 15:35:41 -0300

    Implement new requirements
    Creates buttons for each selection, display score, round winner,
    end game score.

--- js/script.js
+++ js/script.js
@@ -1,116 +1,113 @@
-
 document.addEventListener("DOMContentLoaded", function () {
+  const ROCK = "Rock";
+  const PAPER = "Paper";
+  const SCISSORS = "Scissors";
+  const options = [ROCK, PAPER, SCISSORS];
 
-    const ROCK = 'Rock';
-    const PAPER = 'Paper';
-    const SCISSORS = 'Scissors';
-    const options = [ROCK, PAPER, SCISSORS];
-    let playerScore = 0, computerScore = 0;
+  let playerScore = 0,
+    computerScore = 0,
+    round = 0;
 
-    function computerPlay() {
-        const index = Math.floor((Math.random() * options.length));
-        return options[index];
+  const rockBtn = document.querySelector("#rock-btn");
+  const paperBtn = document.querySelector("#paper-btn");
+  const scissorsBtn = document.querySelector("#scissors-btn");
+  const resetBtn = document.querySelector("#reset-btn");
+
+  const playerNameUI = document.querySelector("#player-name");
+  const playerScoreUI = document.querySelector("#player-score");
+  const computerScoreUI = document.querySelector("#computer-score");
+  const roundResultUI = document.querySelector("#round-result");
+  const gameResultUI = document.querySelector("#game-result");
+
+  const rockBtnEvent = () => playRound(ROCK, computerPlay());
+  const paperBtnEvent = () => playRound(PAPER, computerPlay());
+  const scissorsBtnEvent = () => playRound(SCISSORS, computerPlay());
+
+  rockBtn.addEventListener("click", rockBtnEvent);
+  paperBtn.addEventListener("click", paperBtnEvent);
+  scissorsBtn.addEventListener("click", scissorsBtnEvent);
+  resetBtn.addEventListener("click", resetGame);
+
+  function computerPlay() {
+    const index = Math.floor(Math.random() * options.length);
+    return options[index];
+  }
+
+  function playRound(playerSelection, computerSelection) {
+    let roundResult = "";
+    switch (true) {
+      case playerSelection === null || playerSelection === undefined:
+        roundResult = "Invalid option";
+        break;
+      case playerSelection === computerSelection:
+        roundResult = `Tie! ${playerSelection} x ${computerSelection}`;
+        break;
+      case playerSelection === ROCK && computerSelection === PAPER:
+        roundResult = `You Lose! ${PAPER} beats ${ROCK}`;
+        computerScore++;
+        break;
+      case playerSelection === PAPER && computerSelection === SCISSORS:
+        roundResult = `You Lose! ${SCISSORS} beats ${PAPER}`;
+        computerScore++;
+        break;
+      case playerSelection === SCISSORS && computerSelection === ROCK:
+        roundResult = `You Lose! ${ROCK} beats ${SCISSORS}`;
+        computerScore++;
+        break;
+      case playerSelection === ROCK && computerSelection === SCISSORS:
+        roundResult = `You Won! ${ROCK} beats ${SCISSORS}`;
+        playerScore++;
+        break;
+      case playerSelection === PAPER && computerSelection === ROCK:
+        roundResult = `You Won! ${PAPER} beats ${ROCK}`;
+        playerScore++;
+        break;
+      case playerSelection === SCISSORS && computerSelection === PAPER:
+        roundResult = `You Won! ${SCISSORS} beats ${PAPER}`;
+        playerScore++;
+        break;
+      default:
+        roundResult = "Oops! Something went wrong";
+        break;
     }
 
-    function playRound(playerSelection, computerSelection) {
-        let result = '';
-        switch (true) {
-            case (playerSelection === null || playerSelection === undefined):
-                result = 'Invalid option';
-                break;
-            case (playerSelection === computerSelection):
-                result = `Tie! ${playerSelection} x ${computerSelection}`;
-                break;
-            case (playerSelection === ROCK && computerSelection === PAPER):
-                result = `You Lose! ${PAPER} beats ${ROCK}`;
-                computerScore++;
-                break;
-            case (playerSelection === PAPER && computerSelection === SCISSORS):
-                result = `You Lose! ${SCISSORS} beats ${PAPER}`;
-                computerScore++;
-                break;
-            case (playerSelection === SCISSORS && computerSelection === ROCK):
-                result = `You Lose! ${ROCK} beats ${SCISSORS}`;
-                computerScore++;
-                break;
-            case (playerSelection === ROCK && computerSelection === SCISSORS):
-                result = `You Won! ${ROCK} beats ${SCISSORS}`;
-                playerScore++;
-                break;
-            case (playerSelection === PAPER && computerSelection === ROCK):
-                result = `You Won! ${PAPER} beats ${ROCK}`;
-                playerScore++;
-                break;
-            case (playerSelection === SCISSORS && computerSelection === PAPER):
-                result = `You Won! ${SCISSORS} beats ${PAPER}`;
-                playerScore++;
-                break;
-            default:
-                result = 'Oops! Something went wrong';
-                break;
-        }
-        return result;
+    playerScoreUI.textContent = playerScore;
+    computerScoreUI.textContent = computerScore;
+
+    round++;
+    roundResultUI.textContent = `Round ${round}: ${roundResult}`;
+
+    if (playerScore === 5 || computerScore === 5) {
+      gameResultUI.textContent = gameResult();
+
+      rockBtn.removeEventListener("click", rockBtnEvent);
+      paperBtn.removeEventListener("click", paperBtnEvent);
+      scissorsBtn.removeEventListener("click", scissorsBtnEvent);
     }
+  }
 
-    function findSelection(selection) {
-        let option;
-        switch (selection.toUpperCase()) {
-            case ROCK.toUpperCase():
-                option = ROCK;
-                break;
-            case PAPER.toUpperCase():
-                option = PAPER;
-                break;
-            case SCISSORS.toUpperCase():
-                option = SCISSORS;
-                break;
-            default:
-                option = null;
-                break;
-        }
-        return option;
+  function gameResult() {
+    let result = "";
+    if (playerScore > computerScore) {
+      result = `Congratulations! Score - You: ${playerScore}, Computer: ${computerScore}`;
+    } else {
+      result = `You lose! Score - You: ${playerScore}, Computer: ${computerScore}`;
     }
+    return result;
+  }
 
-    function game() {
-        const gameRounds = 5;
-        let round = 1;
-        let keepGoing = true;
-        while (keepGoing) {
-            const playerInput = prompt('Rock, Paper or Scissors?');
-            if (playerInput === null || playerInput === undefined) {
-                continue;
-            }
+  function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 0;
 
-            const playerPlay = findSelection(playerInput);
-            if (playerPlay === null) {
-                console.log(`Invalid input: ${playerInput}`);
-                continue;
-            }
+    playerScoreUI.textContent = playerScore;
+    computerScoreUI.textContent = computerScore;
+    roundResultUI.textContent = "";
+    gameResultUI.textContent = "";
 
-            const roundResult = playRound(playerPlay, computerPlay());
-            console.log(`Round ${round}: ${roundResult}`);
-
-            round++;
-
-            if (round > gameRounds) {
-                console.log(gameResult());
-                keepGoing = false;
-            }
-        }
-    }
-
-    function gameResult() {
-        let result = '';
-        if (playerScore === computerScore) {
-            result = `Tied! Score - You: ${playerScore}, Computer: ${computerScore}`;
-        } else if (playerScore > computerScore) {
-            result = `Congratulations! Score - You: ${playerScore}, Computer: ${computerScore}`;
-        } else {
-            result = `You lose! Score - You: ${playerScore}, Computer: ${computerScore}`;
-        }
-        return result;
-    }
-
-    game();
-
+    rockBtn.addEventListener("click", rockBtnEvent);
+    paperBtn.addEventListener("click", paperBtnEvent);
+    scissorsBtn.addEventListener("click", scissorsBtnEvent);
+  }
 });
